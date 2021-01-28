d5c94c22dabd286f0c5f9fa239ecb2333d0d2238
Sean Sweda <sweda@ibl.org>
2021-01-28 00:34:15 -0500

    scan.sh: add flags (dftu)
    -d: sort by date
    -f: only flagged
    -t: only trashed
    -u: only unseen

--- bin/scan.sh
+++ bin/scan.sh
@@ -1,19 +1,48 @@
 #!/bin/bash
+# -d: sort by date
+# -f: only flagged
+# -t: only trashed
+# -u: only unseen
 
 export MBLAZE_PAGER="less +G"
 
+lflags="-"
+sorted=by_thread
+scanf="%c%u%r %-3n %10d %17f %t %2i%s"
+
 by_date() {
-    mlist -t `readlink "$FOLDER"` | msort -d | mseq -Sf | mscan
+    msort -d | mseq -Sf | mscan -f "$scanf"
 }
 
 by_thread() {
-    mlist -t `readlink "$FOLDER"` | mthread | mseq -Sf | mscan
+    mthread | mseq -Sf | mscan -f "$scanf"
 }
 
-if [ $# -eq 0 ]; then
-    by_thread
-else
+while getopts dftu opt; do
+    case $opt in
+    d)	sorted=by_date
+	;;
+    f)	lflags+="F"	    # flagged
+	;;
+    t)	lflags+="T"	    # trashed
+	;;
+    u)	lflags+="s"	    # unseen
+	;;
+    *)	echo "usage $0: [ -dftu ]"
+	exit 1
+    esac
+done
+shift $(( $OPTIND - 1 ))
+
+if [ $# -gt 0 ]; then
     ${MHBLAZE_BIN}/folder.sh "$1"
-    by_thread
 fi
+
+# no trashed unless set in lflags
+echo $lflags | grep -q "T"
+if [ $? -ne 0 ]; then
+    lflags+="t"
+fi
+
+mlist $lflags `readlink "$FOLDER"` | $sorted
 
