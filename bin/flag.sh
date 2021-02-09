b9842ed6628ae05c479a4d199aa4877d6b40399c
Sean Sweda <sweda@ibl.org>
2021-02-09 14:42:02 -0500

    implement flag (mflag wrapper, keep seq intact)
    add MH alias 'rmm' (set trashed flag)

---
+++ bin/flag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+myflags='-v'
+
+while getopts DFPRSTdfprst opt; do
+    case $opt in
+    D|F|P|R|S|T|d|f|p|r|s|t)  myflags+="$opt"
+	;;
+    *)	echo "usage $0: [ -DFPRST | -dfprst ] <msg>"
+	exit 1
+    esac
+done
+shift $(( $OPTIND - 1 ))
+
+if [ $# -eq 0 ]; then
+    echo "usage $0: [ -DFPRST | -dfprst ] <msg>"
+    exit 1
+fi
+
+mflag ${myflags} $* | mseq -Sf >/dev/null
+
