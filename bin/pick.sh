d6ec51f94a5a4b43f0ae4ab5296f58cc42f93347
Sean Sweda <sweda@ibl.org>
2021-02-09 16:11:11 -0500

    implement pick (-Alt)
    -A: all folders
    -l: list filenames
    -t: include trashed

---
+++ bin/pick.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# -A: all folders
+# -l: list filenames
+# -t: include trashed
+
+mu index --lazy-check --nocleanup -q
+folder=`readlink $FOLDER | sed -e "s_${INBOX}_/_"`
+folder="maildir:${folder}"
+
+list=''
+trashed="not flag:trashed"
+scanf="%c%u%r %-3n %10d %17f %t %2i%s"
+
+while getopts Alt opt; do
+    case $opt in
+    A)	folder="not maildir:/.expunged"
+	scanf="%-3n %15F %10d %17f %s"
+	;;
+    l)	list="yes"
+	;;
+    t)	trashed=""
+	;;
+    *)	echo "usage $0: [ -DFPRST | -dfprst ] <msg>"
+	exit 1
+    esac
+done
+shift $(( $OPTIND - 1 ))
+
+if [ -z "$list" ]; then
+    mu find -f l ${folder} ${trashed} $* | msort -d | mseq -Sf | mscan -f "$scanf"
+else
+    mu find -f l ${folder} ${trashed} $*
+fi
+
